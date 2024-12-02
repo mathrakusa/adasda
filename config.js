9638db201da6d0f23d5c01cb573d95e15de59ad0
Solo Developer / <nirmalgamagek@gmail.com>
2024-12-02 07:11:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=WzozBIKA#76z8VhzYrfoL9pIhFm731YT9VjbZnVPUamqC8bY65d8",
+SESSION_ID: process.env.SESSION_ID || "YXFTQRwZ#Ms74L5b2eRkc7bwLQMU_guscJeN9kao6wf8EEZMEMr4",
 MONGODB: process.env.MONGODB || "mongodb+srv://athulakumara604:qBwvqo6IM64eT1SL@cluster0.wr7rx.mongodb.net/",
 GITHUB_USERNAME: process.env.GITHUB_USERNAME === undefined ? 'Asithabzkejx': process.env.GITHUB_USERNAME,
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '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': process.env.GITHUB_AUTH_TOKEN
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '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': process.env.GITHUB_AUTH_TOKEN
 };
