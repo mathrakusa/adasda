0778968fc9125a47b51a1cfd53ce43389a3d465e
mathrakusa <praneethmanusha8@gmail.com>
2024-12-04 14:32:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=PuRHkLSB#O3XSDnN3O5IaYrjXXlz0UQ30XZEwjMJIyop7RTNfbgM",
+SESSION_ID: process.env.SESSION_ID || "rcJVkSaI#aIe8WNkunNgXRnS74-jKPavKZsVWXSKieq8fTbms77E",
 MONGODB: process.env.MONGODB || "mongodb+srv://athulakumara604:qBwvqo6IM64eT1SL@cluster0.wr7rx.mongodb.net/",
 GITHUB_USERNAME: process.env.GITHUB_USERNAME === undefined ? 'mathrakusa': process.env.GITHUB_USERNAME,
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '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': process.env.GITHUB_AUTH_TOKEN
